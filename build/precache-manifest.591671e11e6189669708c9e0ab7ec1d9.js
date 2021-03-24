f93e3c12a658320d1b7a4545fdfff69acc76141c
maxim <horamax@yandex.ru>
2021-03-24 16:18:53 +0300

    Kostya pidar

---
+++ build/precache-manifest.591671e11e6189669708c9e0ab7ec1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523b391a91bd9d51f01b240af71b537e",
+    "url": "/LocardsSales/index.html"
+  },
+  {
+    "revision": "8d4cb9b89c116362b47d",
+    "url": "/LocardsSales/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a2442b5239138898d14",
+    "url": "/LocardsSales/static/js/2.9b37f83b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/LocardsSales/static/js/2.9b37f83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4cb9b89c116362b47d",
+    "url": "/LocardsSales/static/js/main.a8556f84.chunk.js"
+  },
+  {
+    "revision": "9fb1e4564075a2bbb7e8",
+    "url": "/LocardsSales/static/js/runtime-main.eadfe92c.js"
+  }
+]);
