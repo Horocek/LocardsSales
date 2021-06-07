0281266f2642964f2353a189b42117cdc5b1be6e
maxim <horamax@yandex.ru>
2021-06-07 15:38:06 +0300

    build

--- build/precache-manifest.fcaae2f56a5f59b25109d70f8fbabe32.js
+++ build/precache-manifest.fcaae2f56a5f59b25109d70f8fbabe32.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2da043fe2a5541d6a396d07c2d60bb49",
+    "revision": "2905bfb47aa30283a5265332771a5a5c",
     "url": "/LocardsSales/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/LocardsSales/static/css/2.b47ae72f.chunk.css"
   },
   {
-    "revision": "b52beae6b76b736ec97e",
-    "url": "/LocardsSales/static/css/main.5f361e03.chunk.css"
+    "revision": "47fb3da959f344a974d9",
+    "url": "/LocardsSales/static/css/main.558d7d39.chunk.css"
   },
   {
     "revision": "ea11d8de097cea5621f1",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/LocardsSales/static/js/2.4ed8986b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b52beae6b76b736ec97e",
-    "url": "/LocardsSales/static/js/main.300a2d22.chunk.js"
+    "revision": "47fb3da959f344a974d9",
+    "url": "/LocardsSales/static/js/main.31a93ca4.chunk.js"
   },
   {
     "revision": "9fb1e4564075a2bbb7e8",
